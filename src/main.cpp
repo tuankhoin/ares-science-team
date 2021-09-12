52b9381ad09650f86a1b0e817d72a430a048a6bf
tuankhoin <tuankhoin@student.unimelb.edu.au>
2021-09-12 18:31:10 +1000

    test commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,19 @@
 #include <Wire.h>
 // #include <distanceSensor.h>
 #include <main.h>
+#include <Servo.h>  
+
+Servo sv;
+int curr,d;
 
 void setup()
 {
   using namespace Main;
   Serial.begin(115200);
 
-  // Init the ToF sensor
+  // Attach servo pin
+  sv.attach(9);
+  // Init the ToF sensors
   tofSensor.init();
   
   Serial.println("new program");
@@ -18,6 +24,15 @@ void loop()
 {
   using namespace Main;
 
+  curr = sv.read();
+  d = tofSensor.read();
+
+  if (d<1000 && d>100 && curr<270) {
+    sv.write(curr+1);
+  } else if (d<=100 && curr>0) {
+    sv.write(curr-1);
+  }
+
   // Print measurement for ToF sensor
-  Serial.println("Distance measurement: " + String(tofSensor.read()) + " mm");
+  Serial.println("Distance measurement: " + String(d) + " mm");
 }
