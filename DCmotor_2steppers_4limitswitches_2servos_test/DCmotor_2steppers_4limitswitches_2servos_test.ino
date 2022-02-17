c9e9a4f09d991d34ca381800c9d6cebfc600a240
Ze Kai Cheong <ernest.cheong.ec@gmail.com>
2022-02-17 20:20:20 +1100

    Added 1 distance sensor

--- DCmotor_2steppers_4limitswitches_2servos_test/DCmotor_2steppers_4limitswitches_2servos_test.ino
+++ DCmotor_2steppers_4limitswitches_2servos_test/DCmotor_2steppers_4limitswitches_2servos_test.ino
@@ -1,8 +1,22 @@
+// Distance sensor
+#include <ComponentObject.h>
+#include <RangeSensor.h>
+#include <SparkFun_VL53L1X.h>
+#include <vl53l1x_class.h>
+#include <vl53l1_error_codes.h>
+#include <Wire.h>
+#include "SparkFun_VL53L1X.h"
+//Optional interrupt and shutdown pins.
+#define SHUTDOWN_PIN 2
+#define INTERRUPT_PIN 3
+SFEVL53L1X distanceSensor;
+//Uncomment the following line to use the optional shutdown and interrupt pins.
+//SFEVL53L1X distanceSensor(Wire, SHUTDOWN_PIN, INTERRUPT_PIN);
+
+// Servo
 #include <Servo.h>
 Servo container_servo;
 Servo probe_servo;
-
-// Servo
 #define CONTAINER_SERVO_PIN 9
 #define PROBE_SERVO_PIN 10
 
@@ -42,8 +56,19 @@ char S2_status;
 
 
 void setup() {
+  Wire.begin();
+  
   // For command inputs
   Serial.begin(9600); 
+
+  // Distance sensor
+  if (distanceSensor.begin() != 0) //Begin returns 0 on a good init
+  {
+    Serial.println("Sensor failed to begin. Please check wiring. Freezing...");
+    while (1)
+      ;
+  }
+  Serial.println("Sensor online!");
 
   // Servo
   container_servo.attach(CONTAINER_SERVO_PIN);
@@ -211,4 +236,22 @@ void loop() {
     probe_servo.write(120);   // Rotate to 0°
   }
 
+
+  // Print distance
+  Serial.println("Distance(mm): ");
+  Serial.print(findDistance());
+  
+}
+
+int findDistance() {
+  distanceSensor.startRanging(); //Write configuration bytes to initiate measurement
+  while (!distanceSensor.checkForDataReady())
+  {
+    delay(1);
+  }
+  int distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
+  distanceSensor.clearInterrupt();
+  distanceSensor.stopRanging();
+
+  return distance;
 }
