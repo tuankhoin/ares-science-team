524e88ee67a0392fb62930d20f8ecc7de69fede3
Ze Kai Cheong <ernest.cheong.ec@gmail.com>
2022-02-20 13:36:26 +1100

    2 distance sensors on the same I2C bus
    Need to change the I2C address for one of the sensors everytime the arduino is powered on or the serial monitor is closed & reopened.

--- Example1_ReadDistance/Example1_ReadDistance.ino
+++ Example1_ReadDistance/Example1_ReadDistance.ino
@@ -3,61 +3,75 @@
 #include <SparkFun_VL53L1X.h>
 #include <vl53l1x_class.h>
 #include <vl53l1_error_codes.h>
-
-/*
-  Reading distance from the laser based VL53L1X
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: April 4th, 2018
-  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
-
-  SparkFun labored with love to create this code. Feel like supporting open source hardware?
-  Buy a board from SparkFun! https://www.sparkfun.com/products/14667
-
-  This example prints the distance to an object.
-
-  Are you getting weird readings? Be sure the vacuum tape has been removed from the sensor.
-*/
-
 #include <Wire.h>
-#include "SparkFun_VL53L1X.h" //Click here to get the library: http://librarymanager/All#SparkFun_VL53L1X
+//#include "SparkFun_VL53L1X.h" //Click here to get the library: http://librarymanager/All#SparkFun_VL53L1X
 
 //Optional interrupt and shutdown pins.
 #define SHUTDOWN_PIN 2
 #define INTERRUPT_PIN 3
 
-SFEVL53L1X distanceSensor;
+SFEVL53L1X S1_distance_sensor;
+SFEVL53L1X S2_distance_sensor;
 //Uncomment the following line to use the optional shutdown and interrupt pins.
 //SFEVL53L1X distanceSensor(Wire, SHUTDOWN_PIN, INTERRUPT_PIN);
+
+int S1_distance;
+int S2_distance;
 
 void setup(void)
 {
   Wire.begin();
 
   Serial.begin(9600);
-  Serial.println("VL53L1X Qwiic Test");
 
-  if (distanceSensor.begin() != 0) //Begin returns 0 on a good init
-  {
-    Serial.println("Sensor failed to begin. Please check wiring. Freezing...");
-    while (1)
-      ;
-  }
-  Serial.println("Sensor online!");
+  S1_distance_sensor.begin();
+  S2_distance_sensor.begin();
+  
+  Serial.println("Please remove S2's SDA & SCL pins. Press any key and hit enter to continue.");
+  while (Serial.available() == 0);
+  Serial.println("Changing S1's I2C address from 82 to 81.");
+  S1_distance_sensor.setI2CAddress(81);
+
+  Serial.print("S1's address: ");
+  Serial.println(S1_distance_sensor.getI2CAddress());
+  Serial.print("S2's address: ");
+  Serial.println(S2_distance_sensor.getI2CAddress());  
+  
+  Serial.println("Please reinsert S2's SDA & SCL pins. Press any key and hit enter to continue.");
+  while (Serial.available() <= 1);
+  Serial.println("Done"); 
+  
 }
 
 void loop(void)
 {
-  distanceSensor.startRanging(); //Write configuration bytes to initiate measurement
-  while (!distanceSensor.checkForDataReady())
+
+  // S1
+  S1_distance_sensor.startRanging(); //Write configuration bytes to initiate measurement
+  while (!S1_distance_sensor.checkForDataReady())
   {
     delay(1);
   }
-  int distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
-  distanceSensor.clearInterrupt();
-  distanceSensor.stopRanging();
+  S1_distance = S1_distance_sensor.getDistance(); //Get the result of the measurement from the sensor
+  S1_distance_sensor.clearInterrupt();
+  S1_distance_sensor.stopRanging();
 
-  Serial.println("Distance(mm): ");
-  Serial.print(distance);
+  Serial.print("S1 Distance(mm): ");
+  Serial.println(S1_distance);
+
+  
+
+  // S2
+  S2_distance_sensor.startRanging(); //Write configuration bytes to initiate measurement
+  while (!S2_distance_sensor.checkForDataReady())
+  {
+    delay(1);
+  }
+  S2_distance = S2_distance_sensor.getDistance(); //Get the result of the measurement from the sensor
+  S2_distance_sensor.clearInterrupt();
+  S2_distance_sensor.stopRanging();
+
+  Serial.print("S2 Distance(mm): ");
+  Serial.println(S2_distance);
 
 }
